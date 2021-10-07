223e116766eafaea535454200366683073ed7ff3
nekumelon <nekumelon@gmail.com>
2021-10-07 14:08:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -34,47 +34,47 @@ class Cnum {
     }
 }
 
-function f(x) { 
-    return x.mul(x).mul(x).sub(new Cnum(1, 0)); 
+function f(x) { // f(x) = x^2 - 4
+    return x.mul(x).sub(new Cnum(4, 0)); 
 }
 
-function fPrime(x) { // f'(x)
-    return new Cnum(3, 0).mul(x).mul(x); 
+function fPrime(x) { // f'(x) = 2x
+    return new Cnum(2, 0).mul(x); 
 }
 
-function newtonRaphson(x) { 
-    return x.sub(f(x).div(fPrime(x))); // Xn + 1 = Xn - f(Xn) / f'(Xn)
+function newtonsMethod(x) { 
+    return x.sub(f(x).div(fPrime(x))); // Xn+1 = Xn-f(Xn)/f'(Xn)
 }
 
 let canvas = document.getElementsByTagName("canvas")[0];
 let context = canvas.getContext("2d");
 let width = window.innerWidth, height = window.innerHeight;
 let centerX = width / 2, centerY = height / 2;
-let zoomX = 1000, zoomY = 1000;
+let scale = 250;
 let error = 1e-2;
 let maxIterations = 100;
 canvas.width = width, canvas.height = height;
 
-for (let x = -centerX; x < centerX; x++) {
-    for (let y = -centerY; y < centerY; y++) {
-        let scaled = new Cnum(x / zoomX, y / zoomY);
-        let root = newtonRaphson(scaled);
-        let x1 = newtonRaphson(root);
+for (let x = -centerX; x < centerX; x++) { // real
+    for (let y = -centerY; y < centerY; y++) { // imagainary
+        let scaled = new Cnum(x / scale, y / scale); // initial guess -Cx - Cxi
+        let zero = newtonsMethod(scaled);
+        let x1 = newtonsMethod(zero);
         let iterations = 0;
 
-        while (Math.abs(x1.real - root.real) > error && iterations <= maxIterations){ // Newtons method
-            root = x1;
-            x1 = newtonRaphson(x1);
+        while (Math.sqrt(x1.real ^ 2 + zero.real ^ 2 + x1.imaginary ^ 2 + zero.imaginary ^ 2) > error && iterations < maxIterations) {
+            zero = x1;
+            x1 = newtonsMethod(x1);
             iterations++;
         }
 
         let alpha = iterations / 20;
         context.fillStyle = "rgba(121, 38, 255, " + alpha + ")";
 
-        if (root.real > 1) {
-            context.fillStyle = "rgba(255, 38, 100, " + alpha + ")";
+        if (zero.real > 1) {
+            context.fillStyle = "rgba(255, 38, 89, " + alpha + ")";
         }
 
-        context.fillRect(x + centerX, y + centerY, 1, 1);
+        context.fillRect(x + centerX, y + centerY, 1, 1); // centered point
     }
 }
